8b55760b5fab8ff30c1b7746ebac000dd7a54f96
MaxStrikanov <maxvell.88@mail.ru>
2024-10-09 17:23:19 +0300

    Внес правки в меню

--- main.js
+++ main.js
@@ -2,5 +2,33 @@ import 'reset-css';
 import './style.css';
 
 
+const tabsTribune = document.querySelectorAll('.tab__item')
+const tabsContentInter = document.querySelectorAll('.menu__item')
+
+console.log(tabsTribune);
 
 
+const changeTabs = (tabs, tabsContent) => {
+  for (let i = 0; i < tabs.length; i++) {
+     
+      tabs[i].addEventListener("click", () => {
+        
+       
+        
+      tabs.forEach((item) => {
+          item.classList.remove("active");
+        });
+      
+      
+      tabsContent.forEach((item) => {
+           item.classList.remove("active");
+       });
+        
+        tabs[i].classList.add("active");
+        tabsContent[i].classList.add("active");
+      });
+    } 
+  }
+
+
+  changeTabs(tabsTribune, tabsContentInter);
